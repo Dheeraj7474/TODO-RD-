f115df92499d3f14bce76c0030716d5a23291885
Dheeraj7474 <112988012+Dheeraj7474@users.noreply.github.com>
2024-07-19 00:37:33 -0500

    Update urls.py

--- todoproject/todoproject/urls.py
+++ todoproject/todoproject/urls.py
@@ -4,5 +4,4 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('todos.urls')),
-    path('api/', RedirectView.as_view(url='/api/todos/', permanent=False)),
 ]
