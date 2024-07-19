17f479d22fc14fd114d41a3a6eb41bbe82858689
Dheeraj7474 <112988012+Dheeraj7474@users.noreply.github.com>
2024-07-19 00:37:08 -0500

    Update urls.py

--- todoproject/todos/urls.py
+++ todoproject/todos/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from .views import get_todos, add_todo, update_todo, delete_todo, api_root
 
 urlpatterns = [
-    path('', api_root, name='api_root'),  # Add this line
-    path('todos/', get_todos, name='get_todos'),
-    path('todos/add/', add_todo, name='add_todo'),
-    path('todos/update/<int:pk>/', update_todo, name='update_todo'),
-    path('todos/delete/<int:pk>/', delete_todo, name='delete_todo'),
+    path('', get_todos, name='get_todos'),
+    path('add/', add_todo, name='add_todo'),
+    path('update/<int:pk>/', update_todo, name='update_todo'),
+    path('delete/<int:pk>/', delete_todo, name='delete_todo'),
 ]
