8e5216b250ece057c37714576623922e60f6729a
Dheeraj7474 <112988012+Dheeraj7474@users.noreply.github.com>
2024-07-22 19:10:30 -0500

    Update urls.py

--- todoproject/todoproject/urls.py
+++ todoproject/todoproject/urls.py
@@ -1,5 +1,6 @@
+# todoproject/urls.py
+from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
