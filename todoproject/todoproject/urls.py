06ad3d216d614c570c98153dd4d869ece5ec09e5
Dheeraj7474 <112988012+Dheeraj7474@users.noreply.github.com>
2024-07-22 19:11:41 -0500

    Update urls.py

--- todoproject/todoproject/urls.py
+++ todoproject/todoproject/urls.py
@@ -1,8 +1,10 @@
 # todoproject/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from . import views  # Import your views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('todos.urls')),
+    path('', views.home, name='home'),  # Add this line
 ]
